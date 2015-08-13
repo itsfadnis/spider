c80c04a75ed41e8e8348086b4897bf9a42eb8d56
Nikhil Fadnis <nik.fadnis@gmail.com>
2015-08-13 17:47:33 +0530

    Removed unnecessary dom access

--- public/js/helper.js
+++ public/js/helper.js
@@ -1,21 +1,12 @@
 var Helper = (function() {
-  $('th').hide();
-
+  
   var hideSpinner = function() {
     $('form').show();
-    $('th').show();
-    $('tbody').show();
-    $('p').show();
-    $('ul').show();
     $('#spinner').addClass('hidden');
   }
-
+  
   var showSpinner = function() {
     $('form').hide();
-    $('th').hide();
-    $('tbody').hide();
-    $('p').hide();
-    $('ul').hide();
     $('#spinner').removeClass('hidden');
   }
 
